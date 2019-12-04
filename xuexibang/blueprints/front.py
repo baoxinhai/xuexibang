a9d760ceb38df4f857fe538ed14e5baa1fda5912
Sugar-Coder <13169670256@163.com>
2019-12-04 12:03:23 +0800

    home.html, qna.html, category.html的显示数据传送完毕，下一阶段获取表单数据插入数据库,以及登录用户的信息获取

--- xuexibang/blueprints/front.py
+++ xuexibang/blueprints/front.py
@@ -28,11 +28,15 @@ class current_user:
 @front_bp.route('/home', methods=['GET', 'POST'])
 def home():
     form = HomeForm()
-    if form.validate_on_submit():
-        title = form.title.data
-        description = form.description.data
-        category = form.category.data
-        return redirect(url_for('front.home'))
+    if current_user.is_authenticated:
+        if form.validate_on_submit():
+            title = form.title.data
+            description = form.description.data
+            category = form.category.data
+            # userid = ？？
+            # success！
+            # print ("title = %s, description= %s, categoty = %d" % (title, description, category))
+            return redirect(url_for('front.home'))
     ret = db.get_result({"function" : db.GET_RECOMMEND_QUESTION, "content": {"number": 5}})
     questions = ret["content"]
     return render_template('front/home.html', questions=questions, form=form)
@@ -42,21 +46,29 @@ def home():
 def index():
     return redirect(url_for('front.home'))
 
+
 # 显示某一类的问题页面
 @front_bp.route('/categoty/<int:category_id>')
 def show_category(category_id):
-    return render_template('front/category.html')
+    # number is the total amount of questions displayed
+    catid = category_id
+    ret = db.get_result({"function" : db.GET_QUESTION_BY_CAT, "content" : {"number": 5, "catid" : category_id}})
+    questions = ret["content"]  # 一个list对象
+    return render_template('front/category.html', questions=questions, catid=catid)
+
 
 # 显示单个问题及其回答的页面
 @front_bp.route('/question/<int:question_id>', methods=['GET', 'POST'])
 def show_question(question_id):
+    form = AnswerForm()
     ret = db.get_result({"function" : db.GET_QUESTION_BY_ID, "content" : {"quid" : question_id}})
     question = ret["content"]  # dict对象
     ret = db.get_result({"function" : db.GET_ANSWER_BY_QUID, "content" : {"quid" : question_id}})
     answers = ret["content"]   # list对象
-    return render_template('front/qna.html', question=question, answers=answers)
+    return render_template('front/qna.html', question=question, answers=answers, form=form)
 
 
+# 显示某个用户提出的问题
 @front_bp.route('/myquestion/<int:user_id>', )
 def myquestion(user_id):
     question = "data"
